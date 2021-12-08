e3aec8c10c56f850052c4ea12ae224a4ae1c500b
Benny15Camron <70347332+Benny15Camron@users.noreply.github.com>
2021-12-08 17:51:56 -0600

    Update project2.c

--- project2.c
+++ project2.c
@@ -48,13 +48,13 @@ void available(proc *tp, long unsigned int tot) {
                         av =  tp[i+1].start - (tp[i].start + tp[i].n);
                         if (av != 0){
                                 st = tp[i].start + tp[i].n;
-                                printf("(%lu,%lu)", av, st);
+                                printf("(%lu, %lu)", av, st);
                         }
                   }
                 else if (remainder != 0) {
                         remainder = tot - (tp[i].start + tp[i].n);
                         av = tp[i].start + tp[i].n;
-                        printf("(%lu,%lu)\n", remainder, av);
+                        printf("(%lu, %lu)\n", remainder, av);
                         break;
                 }
                 av = 0;
@@ -68,7 +68,7 @@ void assigned(proc *tp) {
         }
         for (int i = 0; i < sizeof(tp); ++i) {
                 if (tp[i].name[0] != '\0') {
-                        printf("(%s,%lu,%lu)", tp[i].name, tp[i].n, tp[i].start);
+                        printf("(%s, %lu, %lu)", tp[i].name, tp[i].n, tp[i].start);
                 }
         }
         printf("\n");
