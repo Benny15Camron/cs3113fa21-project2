d3a0cf91715c8708c7e5c3e64453a9497115ef0f
Benny15Camron <70347332+Benny15Camron@users.noreply.github.com>
2021-12-09 00:00:11 -0600

    Update project2.c

--- project2.c
+++ project2.c
@@ -183,7 +183,7 @@ void bestfit(char** argv, long unsigned int tot) {
         long unsigned int j;
         long unsigned int n;
         FILE *fd;
-        proc tp[tot];
+        proc tp[1000];
         for (int i = 0; i < 20; ++i){
                 tp[i].name[0] = '\0';
                 tp[i].n = 0;
