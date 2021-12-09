7607e604e4cf23ee2b2e768194fb353dbf6e0e8a
Benny15Camron <70347332+Benny15Camron@users.noreply.github.com>
2021-12-09 07:53:09 -0600

    Update project2.c

--- project2.c
+++ project2.c
@@ -426,7 +426,7 @@ int findNextFit(proc *tp, char *name, int pos, long unsigned int n, long unsigne
                 return 0;
         }
 
-        for (int i = pos; i < allocated(tp) + 1; ++i) {
+        for (int i = pos; i < allocated(tp) + 4; ++i) {
                 if (tp[i+1].name[0] != '\0'){
                         diff = tp[i+1].start - (tp[i].start + tp[i].n);
                         if (diff >= n){
